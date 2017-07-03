bfe966e45ff77f1c34462f75c7ccdd2d4466a483
mpbraun2 <mpbraun2@gmail.com>
2017-07-03 20:07:38 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Arrays_Mike_Braun
---
+++ main.rb
@@ -0,0 +1,24 @@
+arr1 = ["Mike", "Matt", "John", "Mau", "Leibel", "Kamil"]
+arr2 = [3, 4, 6, 10, -2, 10, 5]
+
+#puts arr1[0] //.at or .fetch
+
+#arr1.delete("Matt")  //.delete
+#puts arr1
+
+#arr2.reverse!  //.reverse
+#puts arr2
+
+#puts arr2.length //.length
+
+#puts arr2.sort //.sort
+
+#puts arr1.slice(0..4)  //.slice
+
+#puts arr2.shuffle //.shuffle
+
+#puts arr1.join("-") //.join
+
+#puts arr1.insert(3, 'george') //.insert
+
+#puts arr1.values_at(1, 3, 5) //.values_at
